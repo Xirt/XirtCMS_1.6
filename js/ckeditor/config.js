3cb1afae9c716cb3101d5e26e4cc894bd70bfffb
A. Gideonse <agideonse@xirtcms.com>
2012-01-11 00:37:37 +0100

    Added JavaScript 'ckeditor'

---
+++ js/ckeditor/config.js
@@ -0,0 +1,36 @@
+﻿/*
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+   config.docType = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN">';
+   config.resize_enabled = false;
+   config.uiColor = '#dbe5f1';
+   config.toolbar = 'Full';
+   config.toolbar_Full =
+   [
+      ['Source','-','Save','NewPage','Preview','-','Templates'],
+      ['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print', 'SpellChecker', 'Scayt'],
+      ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+      '/',
+      ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
+      ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
+      ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+      ['Link','Unlink','Anchor'],
+      ['Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak'],
+      '/',
+      ['Styles','Format','Font','FontSize'],
+      ['TextColor','BGColor'],
+      ['Maximize', 'ShowBlocks','-','About']
+   ];
+   
+   config.toolbar_Simple = [
+      ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
+      ['NumberedList','BulletedList','-','Blockquote'],
+      ['Smiley','SpecialChar'],
+      ['Font','FontSize'],
+      ['TextColor','BGColor']
+   ];
+};
---
+++ js/ckeditor/simpleconfig.js
@@ -0,0 +1,21 @@
+﻿/*
+Copyright (c) 2003-2009, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+   config.docType = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN">';
+   config.resize_enabled = false;
+   config.toolbar = 'Simple';
+   config.toolbar_Simple = [
+      ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
+      ['NumberedList','BulletedList','-','Blockquote'],
+      ['Smiley','SpecialChar'],
+      ['Font','FontSize'],
+      ['TextColor','BGColor']
+   ];
+
+//    ['Link','Unlink','Anchor'],
+//    ['Image','Smiley','SpecialChar'],
+};
