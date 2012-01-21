fa9ff29572a9081657b60396d0392fe8cba1148f
A. Gideonse <agideonse@xirtcms.com>
2012-01-21 18:31:13 +0100

    Updated language files (added PDF notification)

--- languages/en-GB.js
+++ languages/en-GB.js
@@ -1,63 +1,75 @@
 var XLang = {
 
-   // Browser warning (IE7 / 8)
-   ieTitle:            'Did you know that your browser is out of date?',
-   ieBody:             'To get the best possible experience using our website we recommend that you upgrade to an other web browser or to a newer version of Internet Explorer. A list of the most popular web browsers can be found below.',
-   ieDownload:         'Just click on the icons to get to the download page:',
-   ieIgnore:           'Attempt to browse this website &raquo;',
-   goFirefox:          'Firefox 3.5+',
-   goChrome:           'Chrome 3+',
-   goInternetExplorer: 'IE 8+',
-   goOpera:            'Opera 9.6+',
-   goSafari:           'Safari 3+',
+   // Update notification (older browsers)
+   update : {
+      'header'       : "Did you know that your browser is out of date?",
+      'introduction' : "To get the best possible experience using our website we recommend that you upgrade to a newer version of one of the major browsers. A list of the most popular web browsers can be found below.",
+      'download'     : "Click on one of the icons to go to the download page:",
+      'continue'     : "Continue without updating &raquo;",
+      'goFirefox'    : "Firefox 9+",
+      'goChrome'     : "Chrome 16+",
+      'goExplorer'   : "IE 8+",
+      'goOpera'      : "Opera 11+",
+      'goSafari'     : "Safari 5+"
+   },
+   
+   // PDF Reader
+   reader : {
+      'header'       : "Downloading a PDF file",
+      'introduction' : "The file you are trying to download is in Portable Document Format (PDF). You will need a (free) PDF reader to open and view the contents of this file. You can download Adobe Reader through the button below if you do not yet have such software installed.",
+      'download'     : "Download Adobe Reader",
+      'alternative'  : "Alternatively, you can also continue to the requested file by clicking 'Continue' if you already have a PDF reader installed.",
+      'continue'     : "Continue",
+      'cancel'       : "Cancel"
+   },
 
    // System messages
    messages : {
-      system  : 'System notice',
-      process : 'Your request is being processed.',
-      loading : 'The data is being loaded...',
-      saved   : 'The data was saved succesfully.',
-      success : 'Your request has been processed successfully.',
-      removed : 'The item was removed succesfully',
-      failed  : 'Failed to complete request.',
-      empty   : 'There are no items to display'
+      'system'  : "System notice",
+      'process' : "Your request is being processed.",
+      'loading' : "The data is being loaded...",
+      'saved'   : "The data was saved succesfully.",
+      'success' : "Your request has been processed successfully.",
+      'removed' : "The item was removed succesfully",
+      'failed'  : "Failed to complete request.",
+      'empty'   : "There are no items to display"
    },
 
    // XValidator values
    validation: {
-      'required'                 : 'This field is required.',
-      'validate-integer'         : 'Only whole numerical characters allowed.',
-      'validate-digits'          : 'Only numerical characters allowed.',
-      'validate-alpha'           : 'Only alphabetical characters allowed.',
-      'validate-alphanum'        : 'Only alphanumeric characters allowed.',
-      'validate-simple'          : 'Only simple characters (a-Z, 0-9, - _) allowed.',
-      'validate-email'           : 'Please use a valid e-mailaddress.',
-      'validate-url'             : 'Please use a valid webaddress.',
-      'validate-currency-dollar' : 'Please fill in a valid amount.',
-      'validate-phone'           : 'Please fill in a valid phone number.',
-      'validate-match'           : 'This value should match the previous field.',
-      'minLength'                : 'The inserted value is too short.',
-      'maxLenght'                : 'The inserted value is too long.',
-      'validate-password'        : 'Password is not a valid format.',
-      'validate-required-check'  : 'This checkbox is required.',
-      'validate-custom-required' : 'This field is required.',
-      'validate-one-required'    : 'Please select one of the above options.',
-      'validate-cc-num'          : 'Please enter a valid creditcard number.'
+      'required'                 : "This field is required.",
+      'validate-integer'         : "Only whole numerical characters allowed.",
+      'validate-digits'          : "Only numerical characters allowed.",
+      'validate-alpha'           : "Only alphabetical characters allowed.",
+      'validate-alphanum'        : "Only alphanumeric characters allowed.",
+      'validate-simple'          : "Only simple characters (a-Z, 0-9, - _) allowed.",
+      'validate-email'           : "Please use a valid e-mailaddress.",
+      'validate-url'             : "Please use a valid webaddress.",
+      'validate-currency-dollar' : "Please fill in a valid amount.",
+      'validate-phone'           : "Please fill in a valid phone number.",
+      'validate-match'           : "This value should match the previous field.",
+      'minLength'                : "The inserted value is too short.",
+      'maxLenght'                : "The inserted value is too long.",
+      'validate-password'        : "Password is not a valid format.",
+      'validate-required-check'  : "This checkbox is required.",
+      'validate-custom-required' : "This field is required.",
+      'validate-one-required'    : "Please select one of the above options.",
+      'validate-cc-num'          : "Please enter a valid creditcard number."
    },
 
    // Errors & notifications
    errors: {
-      communication: 'A communication error has occured. Please contact an administrator if this problem persists.'
+      communication: "A communication error has occured. Please contact an administrator if this problem persists."
    },
 
    // Time / Date related
-   months: ['January', 'February', 'March','April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-   days  : ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'],
+   months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+   days  : ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"],
 
    // Miscellaneous
    misc: {
-      'xOfY' : 'Image {x} of {y}',
-      'close': 'Close'
+      'xOfY' : "Image {x} of {y}",
+      'close': "Close"
    }
 
 };
--- languages/nl-NL.js
+++ languages/nl-NL.js
@@ -1,26 +1,38 @@
 var XLang = {
 
-   // Browser warning (IE7 / 8)
-   ieTitle:            'Weet u dat uw browser verouderd is?',
-   ieBody:             'Om onze site zo goed mogelijk te kunnen gebruiken, raden wij u daarom aan om een andere browser te installeren of uw huidige browser op te waarderen. Op deze lijst vindt u een lijst van de meest populaire browsers.',
-   ieDownload:         'Klik op een icon om naar de downloadpagina te gaan:',
-   ieIgnore:           'Toch doorgaan &raquo;',
-   goFirefox:          'Firefox 3.5+',
-   goChrome:           'Chrome 3+',
-   goInternetExplorer: 'IE 8+',
-   goOpera:            'Opera 9.6+',
-   goSafari:           'Safari 3+',
+   // Update notification (older browsers)
+   update : {
+      'header'       : 'Wist u dat uw browser verouderd is?',
+      'introduction' : 'Om onze site zo goed mogelijk te kunnen gebruiken, raden wij u daarom aan om een up te graden naar uw favorite browser. Onderstaand vindt u een lijst met de meest populaire browsers.',
+      'download'     : 'Klik op een icoon om naar de downloadpagina te gaan:',
+      'continue'     : 'Doorgaan zonder update &raquo;',
+      'goFirefox'    : 'Firefox 9+',
+      'goChrome'     : 'Chrome 16+',
+      'goExplorer'   : 'IE 8+',
+      'goOpera'      : 'Opera 11+',
+      'goSafari'     : 'Safari 5+'
+   },
+
+   // PDF Reader
+   reader : {
+      'header'       : "Downloaden van PDF bestand",
+      'introduction' : "Het bestand dat u wilt downloaden is in Portable Document Format (PDF). Er is een (gratis) PDF reader nodig om dit bestand te openen. Als u nog geen PDF reader heeft kunt u gratis Adobe Reader via onderstaande knop downloaden.",
+      'download'     : "Download Adobe Reader",
+      'alternative'  : "Indien u al beschikt over dergelijke software kunt u direct het opgevraagde bestand downloaden door op 'Verder gaan' te klikken.",
+      'continue'     : "Verder gaan",
+      'cancel'       : "Annuleren"
+   },
 
    // System messages
    messages : {
-      system  : 'Systeembericht',
-      process : 'Uw opdracht is in behandeling.',
-      loading : 'De data wordt geladen...',
-      saved   : 'De data is succesvol opgeslagen.',
-      success : 'De handeling is succesvol uitgevoerd.',
-      removed : 'Het item is succesvol verwijderd.',
-      failed  : 'De handeling kon niet worden uitgevoerd.',
-      empty   : 'Er geen items gevonden voor weergave'
+      'system'  : 'Systeembericht',
+      'process' : 'Uw opdracht is in behandeling.',
+      'loading' : 'De data wordt geladen...',
+      'saved'   : 'De data is succesvol opgeslagen.',
+      'success' : 'De handeling is succesvol uitgevoerd.',
+      'removed' : 'Het item is succesvol verwijderd.',
+      'failed'  : 'De handeling kon niet worden uitgevoerd.',
+      'empty'   : 'Er geen items gevonden voor weergave'
    },
 
    // XValidator values
