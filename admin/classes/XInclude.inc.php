f2a544e53b0ef8c60df001fdf51285adb1fa9622
A. Gideonse <agideonse@xirtcms.com>
2012-01-28 21:17:21 +0100

    Added backend content engine

---
+++ admin/classes/XInclude.inc.php
@@ -0,0 +1,159 @@
+<?php
+
+/**
+ * Utility class to include XirtCMS page items
+ *
+ * @author     A.G. Gideonse
+ * @version    1.6
+ * @copyright  XirtCMS 2010 - 2012
+ * @package    XirtCMS
+ */
+class XInclude {
+
+
+   /**
+    * @var The default location of plugins (JS)
+    */
+   const PATH_PLUGINS = "../js/plugins/%s/plugin.js";
+
+
+   /**
+    * @var The default location of components
+    */
+   const PATH_COMPONENTS = "./components/%s/index.com.php";
+
+
+   /**
+    * @var The default location of components
+    */
+   const PATH_MANAGERS = "./managers/%s/index.adm.php";
+
+
+
+   /****************************/
+   /* EXTERNAL LOADING METHODS */
+   /****************************/
+
+   /**
+    * Shows the head of the page (title, meta, css, js)
+    */
+   public static function header() {
+      global $xConf, $xPage;
+
+      // Prepare page title
+      $title = implode(array_reverse($xPage->_titles), ' | ');
+
+      // Prepare stylesheets
+      asort($xPage->_stylesheets);
+      $xPage->_stylesheets = array_keys($xPage->_stylesheets);
+
+      // Prepare JavaScript files (combine if requested)
+      if (!$xConf->debugMode && $xConf->combineScripts) {
+
+         $list = array();
+         foreach ($xPage->_scripts as $key => $file) {
+
+            if (strpos($file, '://') === false) {
+
+               unset($xPage->_scripts[$key]);
+               $list[] = $file;
+
+            }
+
+         }
+
+         $xPage->_scripts[] = 'xjs/' . implode(',', $list);
+
+      }
+
+      // Show template
+      $tpl = new XTemplate();
+      $tpl->assign('xConf', $xConf);
+      $tpl->assign('title', $title);
+      $tpl->assign('scripts', $xPage->_scripts);
+      $tpl->assign('metatags', $xPage->_metatags);
+      $tpl->assign('stylesheets', $xPage->_stylesheets);
+      $tpl->display('templates/xtemplates/display-header.tpl');
+
+      $xPage->sentHeaders = true;
+
+   }
+
+
+   /**
+    * Includes a plugin in the page (for JavaScript plugins)
+    *
+    * @param $type The plugin to load
+    */
+   public static function plugin($type) {
+      global $xPage;
+
+      if (!defined('PLUGIN.' . $type)) {
+
+         define('PLUGIN.' . $type, 1);
+         $path = sprintf(self::PATH_PLUGINS, $type);
+
+         // Find plugin
+         if (!file_exists($path)) {
+
+            trigger_error("[XCore] Plugin not found ({$type})", E_USER_NOTICE);
+            return false;
+
+         }
+
+         // Include plugin (header)
+         if (!$xPage->cached['headers']) {
+            return XPage::addScript($path);
+         }
+
+         // Include plugin (content)
+         $tpl = new XTemplate();
+         $tpl->assign('xConf', $xConf);
+         $tpl->assign('script', $path);
+         $tpl->display('templates/xtemplates/display-script.tpl');
+
+      }
+
+   }
+
+
+   /**
+    * Shows the main content of the site (after preloading)
+    */
+   public static function component() {
+      global $xPage;
+
+      if ($xPage->cached['content']) {
+
+         $xPage->cached['content'] = false;
+         print($xPage->_content);
+
+      }
+
+   }
+
+
+   /**
+    * Shows parsing knowledge in debug mode (default behavior)
+    *
+    * @param $ignore When true, statistics are always shown
+    */
+   public static function statistics($ignore = false) {
+      global $xConf, $xDb, $xStart;
+
+      if (!$xConf->debugMode || $ignore) {
+         return false;
+      }
+
+      // Show template
+      $tpl = new XTemplate();
+      $tpl->assign('memoryUse', round(memory_get_peak_usage() / 1048576, 3));
+      $tpl->assign('parseTime', round(XTools::getMicrotime() - $xStart, 3));
+      $tpl->assign('queryTime', round($xDb->timer, 3));
+      $tpl->assign('queryCount', count($xDb->cache));
+      $tpl->display('templates/xtemplates/display-statistics.tpl');
+
+   }
+
+}
+?>
---
+++ admin/classes/XPage.inc.php
@@ -0,0 +1,359 @@
+<?php
+
+/**
+ * Class that loads main content of the requested page
+ *
+ * @author     A.G. Gideonse
+ * @version    1.6
+ * @copyright  XirtCMS 2010 - 2012
+ * @package    XirtCMS
+ */
+class XPage {
+
+   /**
+    * @var The cId of the current page
+    */
+   var $cId = null;
+
+
+   /**
+    * @var List holding all (sub)titles for the page (TITLE-tag)
+    */
+   var $_titles = array();
+
+
+   /**
+    * @var List holding all meta tags for the page
+    */
+   var $_metatags = array();
+
+
+   /**
+    * @var List holding all js files for the page
+    */
+   var $_scripts = array();
+
+
+   /**
+    * @var List holding all css files for the page
+    */
+   var $_stylesheets = array();
+
+
+   /**
+    * @var Variables holding the page content for showing
+    */
+   var $_content = null;
+
+
+   /**
+    * @var Array Keeps status of cached items
+    */
+   var $cached = array();
+
+
+   /**
+    * CONSTRUCTOR
+    */
+   function __construct() {
+
+      $this->cId = XTools::getParam('cid', 0, _INT);
+
+      $this->cached = array(
+         'content' => true,
+         'headers' => true
+      );
+
+   }
+
+
+   /**********************/
+   /* PRELOADING METHODS */
+   /**********************/
+
+   /**
+    * Preloads component and headers
+    *
+    * @param $configureHeaders Toggles default meta information / headers
+    */
+   public function preload($configureHeaders = true) {
+      global $xConf, $xLang;
+
+      if ($configureHeaders) {
+
+         $this->setTitle($xConf->title);
+
+         $this->addInformation('robots',        'noindex, follow');
+         $this->addInformation('language',      $xConf->language);
+         $this->addInformation('generator',     $xLang->version);
+
+         $this->addStylesheet('../templates/xcss/xirt.css', 0);
+         $this->addStylesheet('../templates/xcss/xlist.css', 0);
+
+         $this->addScript(sprintf('languages/%s.js', $xConf->language));
+         $this->addScript('../js/mootools.lib.js');
+         $this->addScript('../js/mootoolsmore.lib.js');
+         $this->addScript('../js/mootoolscustom.lib.js');
+         $this->addScript('../js/xvalidate.lib.js');
+         $this->addScript('../js/xlist.js');
+         $this->addScript('../js/xadmin.lib.js');
+         $this->addScript('../js/xirt.lib.js');
+
+      }
+
+      ob_start();
+      $this->_load();
+      $this->_content = ob_get_clean();
+
+   }
+
+
+   /**
+    * Preloads main component
+    *
+    * @access private
+    */
+   private function _load() {
+      global $xConf, $xUser;
+
+      // Unauthorized requests (login panel)
+      if (!$xUser->isAuth($xConf->adminLevel)) {
+
+         if ($component = $this->_loadManager('adm_login')) {
+
+            if (isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
+               return $component->showAjax();
+            }
+
+            return $component->showNormal();
+         }
+
+         return Xirt::notAuthorized();
+      }
+
+      // Authorized request
+      $component = XTools::getParam('content');
+      switch (substr($component, 0, 3)) {
+
+         case 'adm':
+            $component = $this->_loadManager($component);
+         break;
+
+         case 'com':
+            $component = $this->_loadComponent($component);
+         break;
+
+         default:
+            $component = $this->_loadManager('adm_portal');
+         break;
+
+      }
+
+      switch (true) {
+
+         case ($component === null):
+            return null;
+
+         case array_key_exists('HTTP_X_REQUESTED_WITH', $_SERVER):
+            return $component->showAjax();
+
+         default:
+            return $component->showNormal();
+
+      }
+
+   }
+
+
+
+   /***************************/
+   /* EXTERNAL CONFIG METHODS */
+   /***************************/
+
+   /**
+    * Sets the title
+    *
+    * @param $str String containing the new title
+    */
+   public static function setTitle($str) {
+      global $xPage;
+
+      $xPage->_titles = array($str);
+
+   }
+
+
+   /**
+    * Extends the title
+    *
+    * @param $str String containing the new title extension
+    */
+   public static function extendTitle($str, $prepend = false) {
+      global $xPage;
+
+      if ($prepend && count($xPage->_titles)) {
+         return array_unshift($xPage->_titles, $str);
+      }
+
+      $xPage->_titles[] = $str;
+
+   }
+
+
+   /**
+    * Adds / overwrites META-tag
+    *
+    * @param $type String containing the property (type) of the META-tag
+    * @param $value String containing the value of the META-tag
+    */
+   public static function addInformation($type, $value) {
+      global $xPage;
+
+      if ($type && $value) {
+         $xPage->_metatags[$type] = $value;
+      }
+
+   }
+
+
+   /**
+    * Adds a CSS-tag (with priority)
+    *
+    * @param $file String with path to filename
+    * @param $prio int for setting the priority of the file (defaults 1)
+    */
+   public static function addStylesheet($file, $prio = 1) {
+      global $xPage;
+
+      $xPage->_stylesheets[$file] = $prio;
+   }
+
+
+   /**
+    * Adds a SCRIPT-tag (for JavaScript files)
+    *
+    * @param $file String with path to filename
+    */
+   public static function addScript($file) {
+      global $xPage;
+
+      $xPage->_scripts[] = $file;
+   }
+
+
+
+   /*******************/
+   /* LOADING METHODS */
+   /*******************/
+
+   /**
+    * Loads a manager
+    *
+    * @access private
+    * @param $name String containing the name of the manager to load
+    * @return mixed The new instance of the manager or false / null on failure
+    */
+   private function _loadManager($name) {
+      global $xConf, $xUser;
+
+      // Check component
+      $path = sprintf(XInclude::PATH_MANAGERS, $name);
+      if (!is_readable($path)) {
+
+         trigger_error("[XCore] Manager failure ({$name})", E_USER_WARNING);
+         return Xirt::pageNotFound();
+
+      }
+
+      // Check authorization
+      if (isset($xConf->$name) && !$xUser->isAuth($xConf->$name)) {
+
+         Xirt::notAuthorized();
+         return null;
+
+      }
+
+      require_once($path);
+      return new Manager($name, $component->config);
+   }
+
+
+   /**
+    * Loads a component
+    *
+    * @access private
+    * @param $name String containing the name of the component to load
+    * @return mixed The new instance of the component or false / null on failure
+    */
+   private function _loadComponent($name) {
+      global $xConf, $xUser;
+
+      // Find component
+      $components = Xirt::getComponents();
+      if (!array_key_exists($name, $components)) {
+
+         trigger_error("[Core] Component not found ({$name})", E_USER_WARNING);
+         return Xirt::pageNotFound();
+
+      }
+
+      // Check authorization
+      $component = $components[$name];
+      if (!$xUser->isAuth($component->access_min, $component->access_max)) {
+
+         Xirt::notAuthorized();
+         return null;
+
+      }
+
+      // Check component
+      $path = sprintf(XInclude::PATH_COMPONENTS, $name);
+      if (!is_readable($path)) {
+
+         trigger_error("[XCore] Component failure ({$name})", E_USER_WARNING);
+         return Xirt::pageNotFound();
+
+      }
+
+      require_once($path);
+      return new Component($name, $component->config);
+   }
+
+
+   /**
+    * Loads a module (for AJAX calls)
+    *
+    * @access private
+    * @param $type String containing the name of the module to load
+    * @return mixed The generated instance of the module or false
+    */
+   private function _loadModule($type) {
+      global $xConf, $xUser;
+
+      $list = new XModuleList();
+      $list->filterByType($type);
+      $list = $list->toArray();
+
+      // Find module
+      if (!count($list) || !$module = current($list)) {
+
+         trigger_error("[XCore] Module not found ({$type})", E_USER_WARNING);
+         return false;
+
+      }
+
+      // Check module
+      $path = sprintf(XInclude::PATH_MODULES, $xConf->baseDir, $type);
+      if (!is_readable($path)) {
+
+         trigger_error("Module failure ({$type})", E_USER_WARNING);
+         return false;
+
+      }
+
+      require_once($path);
+      return new $type($module->config);
+   }
+
+}
+?>
