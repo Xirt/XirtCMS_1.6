e174eeda5d1dedd8b0ee5502a6e4d3f2532f0986
A. Gideonse <agideonse@xirtcms.com>
2012-03-13 14:59:10 +0100

    Switched parameters for XTools::getParam(). Fixes #17

--- admin/components/com_content/includes/Manager.php
+++ admin/components/com_content/includes/Manager.php
@@ -20,19 +20,19 @@ class Manager {
 
          'config' => json_encode((object)array(
             'css_name'      => XTools::getParam('item_css_name'),
-            'show_title'    => XTools::getParam('item_show_title',    _INT, 0),
-            'show_author'   => XTools::getParam('item_show_author',   _INT, 0),
-            'show_created'  => XTools::getParam('item_show_created',  _INT, 0),
-            'show_modified' => XTools::getParam('item_show_modified', _INT, 0),
-            'download_icon' => XTools::getParam('item_download_icon', _INT, 0),
-            'print_icon'    => XTools::getParam('item_print_icon',    _INT, 0),
-            'mail_icon'     => XTools::getParam('item_mail_icon',     _INT, 0),
-            'back_icon'     => XTools::getParam('item_back_icon',     _INT, 0)
+            'show_title'    => XTools::getParam('item_show_title',    0, _INT),
+            'show_author'   => XTools::getParam('item_show_author',   0, _INT),
+            'show_created'  => XTools::getParam('item_show_created',  0, _INT),
+            'show_modified' => XTools::getParam('item_show_modified', 0, _INT),
+            'download_icon' => XTools::getParam('item_download_icon', 0, _INT),
+            'print_icon'    => XTools::getParam('item_print_icon',    0, _INT),
+            'mail_icon'     => XTools::getParam('item_mail_icon',     0, _INT),
+            'back_icon'     => XTools::getParam('item_back_icon',     0, _INT)
          ))
 
       );
 
-      $xDb->update('#__components', $data, "com_name = 'com_content'");
+      $xDb->update('#__components', $data, "type = 'com_content'");
 
    }
 
